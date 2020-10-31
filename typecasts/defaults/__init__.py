c62ecef2e463101d6f4805e99c18f74996e0a023
Anatoly Scherbakov <altaisoft@gmail.com>
2020-10-31 18:50:23 +0700

    #2 Optional pydantic support added

---
+++ typecasts/defaults/__init__.py
@@ -0,0 +1,7 @@
+from typecasts.defaults.base import casts
+
+# Configure pydantic rules if Pydantic is installed.
+try:
+    from typecasts.defaults import pydantic
+except ImportError:   # pragma: no cover
+    ...
